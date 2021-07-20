600f2660849b382bfb331c0a44899d2980a010f7
Chadewik-junao <53118074+Chadewik-junao@users.noreply.github.com>
2021-07-20 22:58:39 +0800

    first commit

---
+++ app/src/main/java/com/example/code07/New.kt
@@ -0,0 +1,5 @@
+package com.example.code07
+
+class New(val mTile:String,val mAuther:String,val mImageId:Int) {
+
+}
---
+++ app/src/main/java/com/example/code07/NewAdapter.kt
@@ -0,0 +1,28 @@
+package com.example.code07
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.list_item.view.*
+
+class NewAdapter(activity: Activity,val resourceId:Int,data:List<New>): ArrayAdapter<New>(activity,resourceId,data) {
+    override fun getView(position:Int, convertView:View?, parent:ViewGroup):View{
+        val view=LayoutInflater.from(context).inflate(resourceId,parent,false)
+        val tvTitle:TextView=view.findViewById(R.id.tv_title)
+        val tvAuthor :TextView=view.findViewById(R.id.tv_subtitle)
+        val ivImage:ImageView=view.findViewById(R.id.iv_image)
+        val new: New? =getItem(position)
+        if(new!=null){
+            tvTitle.setText(new.mTile)
+            tvAuthor.setText(new.mAuther)
+            ivImage.setImageResource(new.mImageId)
+        }
+
+
+        return view
+    }
+}
